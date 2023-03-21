839f83872142431e68e3839f899c2e22a06ca4f3
Gomatheeswaran K <87482742+Gomatheeswaran@users.noreply.github.com>
2023-03-21 13:33:04 +0530

    weather finder source codes added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Controller from "./screens/Controller";
+
+function App() {
+  return (
+    <div>
+      <Controller/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+const API_KEY = '1d26aa53f9de6fa01ebdd15e5510bf09';
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+export const getWeather = async (city, country) => {
+    try{
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+                        return response.data;
+    } catch (error){
+        console.log('Error while calling the api', error.message);
+        return error.response;
+    }
+}
